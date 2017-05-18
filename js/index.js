949ed63918487ba002cc8f9e035e9797b6fa1534
yuningsue <yuningsue@gmail.com>
2017-05-18 13:19:25 +0800

    revised

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 $(document).ready(function(e) {
 
 	$('#track-list').on('click', function() {
-		$('.right__panel').toggleClass('display');
-        $('.player').removeClass('display');
-	});
-
+        $(".right__panel").fadeOut(500);
+        $(".player").fadeIn(1000);
+    });
+    
 	$('.close').on('click', function() {
-		$('.player').toggleClass('display');
-		$('.right__panel').removeClass('display');
-		$('.music__menu').removeClass('menu_list_open');
+        $(".player").fadeOut("slow");
+        $(".right__panel").fadeIn("slow");
 	});
 
     $('.infor').on('click', function() {
-        $('.player').toggleClass('display');
+        $(".player"). hide("slide", { direction: "left" }, 300);
     });
 
     $('.lyric-button').on('click', function() {
@@ -38,7 +37,7 @@ function getInitialTracks(a) {
         var tmp = '';
         for (var i = 0; i < r.length; i++) {
          tracknum.push(i);
-          tmp = '<a onclick="getTracks(tracknum[i])" href="#">' + r[i].title + '</a>' + '<div class=musicgenre></div>';
+          tmp = '<a onclick="getTracks(tracknum[i])" href="#">' + r[i].title  +'</a>';
         $("<ul/>").html(tmp).appendTo("#track-list");
       }        
     })
@@ -68,7 +67,7 @@ function attachToPlayer(a) {
         "background-image": "url(" + a.cover + ")"
     }), $("<img/>").attr("src", a.cover).on("load", function() {
         $(".player").addClass("loaded"), $(".loading").addClass("closed"), $(".right__panel").addClass("loaded")
-    }), $(".loved").html('<i class="fa fa-heart" aria-hidden="true"></i> ' + a.love), $(".user-avatar").html('<a href="' + a.userLink + '" target=_blank><img src="' + a.userAvatar + '" alt="' + a.userName + '"></a>'), $(".info").html('<p><a class="userName" href="' + a.userLink + '" target=_blank>' + a.userName + '</a></br><a class="trackName" href="' + a.link + '" target="_black">' + a.name + "</a></p>"), $(".wave-form").css({
+    }), $(".loved").html('<i class="fa fa-heart" aria-hidden="true"></i> ' + a.love), $(".user-avatar").html('<img class="img-circle" src="' + a.userAvatar + '" alt="' + a.userName + '"></a>'), $(".info").html('<p><a class="userName" href="' + a.userLink + '" target=_blank>' + a.userName + '</a></br><a class="trackName" href="' + a.link + '" target="_black">' + a.name + "</a></p>"), $(".wave-form").css({
         "background-image": "url(" + a.waveform + ")"}), $(".userName").html(a.userName), $(".musicgenre").html(a.musicgenre)
 }
 function trackPlay() {
